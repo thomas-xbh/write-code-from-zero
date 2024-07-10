633f68c0681793529319a2146b5ad05777a4beb8
duruo <duruo@dianjia.io>
2024-07-10 11:05:07 +0800

    '数组的度‘

--- src/ArrayType/Solution03.java
+++ src/ArrayType/Solution03.java
@@ -97,9 +97,31 @@ public class Solution03 {
 
     public static int findShortestSubArray01(int[] nums) {
         /*
-        * 改进，还是通过哈希表的方式，只不过是每个数映射一个长度为3的数组，数组中三个元素分别代表这个数出现的次数，第一次出现的下标，和最后一次出现的下标
-        * */
+         * 改进，还是通过哈希表的方式，只不过是每个数映射一个长度为3的数组，数组中三个元素分别代表这个数出现的次数，第一次出现的下标，和最后一次出现的下标
+         * */
+        Map<Integer, int[]> map = new HashMap<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i])) {
+                map.get(nums[i])[0]++;
+                map.get(nums[i])[2] = i;
+            } else {
+                map.put(nums[i], new int[]{1, i, i});
+            }
+        }
+        int maxNum = 0, minLen = 0;
+        for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
+            int[] arr = entry.getValue();
+            if (maxNum < arr[0]) {
+                maxNum = arr[0];
+                minLen = arr[2] - arr[1] + 1;
+            } else if (maxNum == arr[0]) {
+                if (minLen > arr[2] - arr[1] + 1) {
+                    minLen = arr[2] - arr[1] + 1;
+                }
+            }
+        }
 
-        return 11;
+        return minLen;
     }
 }
